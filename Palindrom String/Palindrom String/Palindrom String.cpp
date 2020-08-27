71116fca0266c27d5fbcdfbd34eed7001f2f1048
Zinebo <69069622+Zinebo98@users.noreply.github.com>
2020-08-27 02:17:06 +0200

    Add files via upload

---
+++ Palindrom String/Palindrom String/Palindrom String.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.size();
+
+        while (i < j) {
+
+            while (i < j && !isalnum(s[i]))
+                ++i;
+            while (j > i && !isalnum(s[j]))
+                --j;
+
+            if (tolower(s[i]) != tolower(s[j]))
+                return false;
+
+            ++i, --j;
+        }
+        return true;
+    }
+};
